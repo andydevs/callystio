52da374198ea6d6c17c2dc2d6a6ff39fb2187261
Andydevs <akanshul97@gmail.com>
2021-07-16 22:06:55 -0400

    We're creating a separate system for that

--- snapshot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo '------------------------------------------------BUILDING SITE---------------------------------------------------'
-python -m callystio build
-echo '----------------------------------------------UPDATING SNAPSHOTS------------------------------------------------'
-test -d snapshot && rm -rf snapshot/* || mkdir snapshot
-cp dist/* snapshot/
